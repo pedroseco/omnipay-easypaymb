7e3e85176995dd0000e68b06fbab3da98448e560
Pedro Seco <pedroseco@modular-studio.com>
2017-07-13 15:45:57 +0100

    first commit

---
+++ src/Message/Response.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Omnipay\EasypayMb\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+
+class Response extends AbstractResponse
+{
+	
+    public function isSuccessful()
+    {
+        return true;
+    }
+	
+	public function getMessage()
+    {
+		$data = $this->getData();
+        return $data->resposta;
+    }
+	
+	public function getTransactionDetails()
+    {
+        $data = $this->getData();
+        return $data;
+    }
+    
+}
